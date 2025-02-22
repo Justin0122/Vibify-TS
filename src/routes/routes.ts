51c72e27ac901ec6f9f0bc81cd6216f4fb431b12
Justin Jongstra <justin@justinjongstra.nl>
2025-02-22 22:39:57 +0100

    added scopes to auth url

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,7 +13,7 @@ router.get("/", (_req: Request, res: Response) => {
 });
 
 router.get('/authorize/:userId', catchErrors(async (req: Request, res: Response) => {
-    const url = `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&scope=user-library-read&state=${req.params.userId}`;
+    const url = `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&scope=user-read-email%20user-read-private%20user-library-read%20user-top-read%20user-read-recently-played%20user-read-currently-playing%20user-follow-read%20playlist-read-private%20playlist-modify-public%20playlist-modify-private%20playlist-read-collaborative%20user-library-modify&state=${req.params.userId}`;
     res.redirect(url);
 }));
 
